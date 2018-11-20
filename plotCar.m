44e776f214d364a66ba5270df89a214681e1663f
Larissa Ganaha <larissaganaha@gmail.com>
2018-11-20 00:35:50 -0200

    First Commit

---
+++ plotCar.m
@@ -0,0 +1,51 @@
+function plotCar(x, y)
+  plot([x], [y], 's');
+endfunction
+
+function newCoordinates = calculateStep(curr_x, curr_y, curr_fi, theta)
+  
+  new_fi = curr_fi + theta;
+  new_x = curr_x + 2*cos(new_fi);
+  new_y = curr_y + 2*sin(new_fi);
+  
+  newCoordinates = [new_x, new_y, new_fi];
+endfunction
+
+function init(x_start, y_start)
+  figure();
+  axis([0 100 0 100]);
+  hold();
+  rectangle('Position', [0 0 100 100]);
+  plot([50; 50], [100; 95]);
+  plot([x_start], [y_start], 's');
+endfunction
+
+function walkInCircles(x, y, fi, theta)
+  for c = 1:100
+    pause(0.1);
+    newCoordinates = calculateStep(x, y, fi, theta);
+    x = newCoordinates(1);
+    y = newCoordinates(2);
+    fi = newCoordinates(3);
+    plotCar(x, y);
+  end
+endfunction
+
+function parkBackwards(x, y, fi, theta)
+  for c = 1:1000
+    pause(0.1);
+    new_theta = fuzzyControler(x, y, fi, theta);
+    newCoordinates = calculateStep(x, y, fi, new_theta);
+    x = newCoordinates(1);
+    y = newCoordinates(2);
+    fi = newCoordinates(3);
+    plotCar(x, y);
+  end
+endfunction
+
+x = 50;
+y = 50;
+fi = 0;
+theta = 1;
+init(x, y);
+walkInCircles(x, y, fi, theta);
